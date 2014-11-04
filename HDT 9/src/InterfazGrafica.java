4cc5b4e90cb893f4252c3994e6cbaecb7fc6b401
brandonmenv <brg_mev@hotmail.com>
2014-11-04 17:53:03 -0600

    Create InterfazGrafica.java

---
+++ HDT 9/src/InterfazGrafica.java
@@ -0,0 +1,161 @@
+/**
+ * Principal.java
+ * 
+ * @author Brandon Mendez -13087
+ * @author Alejandro Díaz - 13082
+ * 
+ * Descripcion: interfaz grafica de la resolcion de la hoja de trabajo no.9 de 
+ * Algoritmos y estructura de datos.
+ * 
+ */
+//Importando librerias utiles
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+
+public class InterfazGrafica extends JPanel implements ActionListener,ItemListener{
+	/**ATRIBUTOS**/
+	private static JFrame frame;										//frame principal de la GUI y frame de control de errores
+	private JTextArea areatexto; 
+	private JButton btonRutaMasCorta,btonCentroGrafo, btonModificarGrafo;
+	private static String comRutaMasCorta="rutamascorta",
+                comCentroGrafo="comcentrografo",
+                comModificarGrafo="commodificargrafo";
+	
+	/**
+	* Constructor con un parametro que es un frame de control de errores
+	* @param v El parámetro v es un frame que sirve para mostrarle errores 
+        * de ingreso al usuario
+	*/
+	public InterfazGrafica(JFrame v){		
+		//Construyendo paneles de la interfaz				
+		JComponent panelBanner=panelBanner();											
+		JComponent panelBotones=panelBotones();
+		JComponent panelResultado=panelResultado();
+		
+		add(panelBanner);
+		add(panelBotones);
+		add(panelResultado);
+		
+
+	}	
+	
+	/**
+	* Método que devuelve el panel con los botones para seleccionar la implemetacion del stack
+	* @return JPanel con tres botones
+	*/	  									  							  
+        protected JComponent panelBotones(){
+            JPanel panel = new JPanel(new GridLayout(0,1));									//Creando panel con un Gridlayout con 0 filas 1 columna
+            panel.setBorder(BorderFactory.createTitledBorder(" Seleccione una opcion. ")); 		
+            panel.setPreferredSize(new Dimension(150, 95));     							//Cambiandolo de tamanio
+
+            btonRutaMasCorta = new JButton("Ruta Mas corta");
+            btonRutaMasCorta.setActionCommand(comRutaMasCorta);
+            btonRutaMasCorta.addActionListener(this);												//Asignando listener                        
+            btonCentroGrafo = new JButton ("Centro del Grafo");
+            btonCentroGrafo.setActionCommand(comCentroGrafo);
+            btonCentroGrafo.addActionListener(this);												//Asignando listener                        
+            btonModificarGrafo = new JButton ("Modificar Grafo");
+            btonModificarGrafo.setActionCommand(comModificarGrafo);
+            btonModificarGrafo.addActionListener(this);
+            
+            panel.add(btonRutaMasCorta);
+            panel.add(btonCentroGrafo);
+            panel.add(btonModificarGrafo);
+            
+            return panel;
+	}
+		
+	
+			
+	/**
+	* Método que devuelve el panel con una caja de texto y scroll
+	* @return JPanel con una caja de texto y scroll
+	*/	  									  							  
+        protected JComponent panelResultado(){
+            JPanel panel = new JPanel(new GridLayout(0,1));                                                             //Creando panel con un Gridlayout con 0 filas 1 columna
+            panel.setBorder(BorderFactory.createTitledBorder(" Resultado. ")); 		
+            panel.setPreferredSize(new Dimension(335, 100));     							//Cambiandolo de tamanio
+
+            areatexto = new JTextArea ( 16, 58 );
+            areatexto.setEditable ( false ); // set textArea non-editable
+            JScrollPane scroll = new JScrollPane ( areatexto );
+            scroll.setVerticalScrollBarPolicy ( ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS );
+
+            panel.add ( scroll );
+
+
+            return panel;
+	}	
+	
+	
+			
+	/**
+	* Método que devuelve el panel con el banner que contiene una imagen jpg de una calculadora
+	* @return JPanel con una imagen
+	*/
+	protected JComponent panelBanner(){
+		JPanel panel = new JPanel(new GridLayout(0,1));							//Creando panel con un Gridlayout con 0 filas 1 columna
+		JLabel banner = new JLabel("", JLabel.CENTER);  						//Centrando banner              
+		banner.setOpaque(true);        											//Haciendolo mas opaco
+		banner.setPreferredSize(new Dimension(454, 325));         				//Cambiandolo de tamaÃ±o
+		banner.setIcon(new ImageIcon("guate.jpg")); 							//Creando Imaege icon con parametro de direccion	
+		JPanel bannerPanel = new JPanel(new BorderLayout());      				//Creando panel para banner
+		bannerPanel.add(banner, BorderLayout.CENTER);        					//Centrado y agregando el banner al panel
+		bannerPanel.setBorder(BorderFactory.createTitledBorder("Ciudades Principales de Guatemala")); 	//Colocandole texto al borde			
+		panel.add(bannerPanel, BorderLayout.CENTER);							//AÃ±adiendo al Frame
+		return panel;
+	}
+
+	/**
+	*	GESTIONADOR DE EVENTOS: Permite gestionar todos los eventos de la clase.
+	* @see ActionEvent
+	* @see ActionListener
+	*/						  
+	public void actionPerformed(ActionEvent e){									//Empieza el control de eventos
+		String comando = e.getActionCommand();										//String del comando de accion			
+		
+		
+	}//Final de gestionador de eventos	
+
+	/**
+	* creandoInterfaz:Metodo que crea el Frame principal, el contenedor principal y muestra la interfaz grafica
+	* @see windowActivated */
+	public void creandoInterfaz() {
+		frame = new JFrame(" Hoja de Trabajo No.9 Algoritmos y Estructura de Datos. ");			        						//Creando la ventana principal
+		frame.setPreferredSize(new Dimension(580, 510)); 						//colocar caracteristicas al frame (tamaÃ±o)
+		
+		this.setOpaque(true); 													//Se vuelve opaco el panel de contenido para que no sea diferenciado
+		frame.setContentPane(this);												//Estableciendo el panel principal.
+		frame.setResizable(false);												//Bloqueando la maximizaciÃ³n de la ventana
+		
+		frame.setDefaultCloseOperation (WindowConstants.DO_NOTHING_ON_CLOSE);	//Asegurandose de que el evento foco se valla al Panel principal
+		frame.addWindowListener(new WindowAdapter() {							//El evento foco se centra en este panel no importa que
+		//Cerrando con confirmacion
+		public void windowClosing(WindowEvent we){
+			int eleccion = JOptionPane.showConfirmDialog(null, "¿Desea salir?");
+				if ( eleccion == 0) {
+					System.exit(0);
+				}  
+			}
+		});
+		
+		//Mostrando el frame
+		frame.pack();
+		frame.setLocationRelativeTo(null);;										//Frame en el centro
+		frame.setVisible(true);	
+	}
+
+	
+	/*
+	 * Metodo donde se controlan los indices del Combo Box
+	 * Ademas en cada indice se muestra el resultado deseado segun la implementacion escogida por le usuario
+	 * (non-Javadoc)
+	 * @see java.awt.event.ItemListener#itemStateChanged(java.awt.event.ItemEvent)
+	 */
+	public void itemStateChanged(ItemEvent a){ 
+
+			
+	}
+}
